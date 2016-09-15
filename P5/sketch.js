6832ac553034b0ba21e52c95597ef3e783507c79
Daniel Shteremberg <dshteremberg@smu.edu>
2016-09-15 11:08:28 -0500

    working ik chain
    working Ik chain next to see if we can integrate a db so we can control
    this through a different device.

--- P5/sketch.js
+++ P5/sketch.js
@@ -1,22 +1,63 @@
+var j0, j1, j2, j3;
 var lineLength, centerX, centerY;
+var joints = [];
+var jointCount;
+var mx, my;
+var isDraggable;
 
 function setup() {
     createCanvas(windowWidth, windowHeight); //make panel to draw on in the site
-    centerY = height/2;
     centerX = width/2;
+    centerY = height/2;
     lineLength = 100;
+    jointCount = 4;
+    for (var i = 0; i < jointCount; i++) {
+        joints[i] = createVector(lineLength*i, 0);
+    }
 }
 
 function draw() {
     background(100);
-    ellipse(centerX, centerY, 10, 10);
-    var a = atan2(mouseY - centerY, mouseX - centerX);
-    var newX = centerX + cos(a) * lineLength;
-    var newY = centerY + sin(a) * lineLength;
-    ellipse(newX, newY, 10, 10);
-    a = atan2(newY, newX);
-    newX = newX + cos(a) * lineLength;
-    newY = newY + sin(a) * lineLength;
-    ellipse(newX, newY, 10, 10);
+    noFill();
+    mx = mouseX - centerX;
+    my = mouseY - centerY;
+    translate(centerX, centerY);
+    IkPoints();
+    drawPoints();
+}
 
+function IkPoints() {
+    for (var i = joints.length - 2; i >= 0; i--) {
+        var theta = atan2(joints[i+1].y - joints[i].y, joints[i+1].x - joints[i].x);
+        joints[i].x = joints[i+1].x - cos(theta) * lineLength;
+        joints[i].y = joints[i+1].y - sin(theta) * lineLength;
+    }
+
+    if(isDraggable) {
+        joints[joints.length-1].x = mx;
+        joints[joints.length-1].y = my;
+    }
+
+}
+
+function drawPoints(){
+    beginShape();
+    for (var i = 0; i < joints.length; i++) {
+        vertex(joints[i].x, joints[i].y);
+    }
+    endShape();
+    fill(255);
+    for (var i = 0; i < joints.length; i++) {
+        ellipse(joints[i].x, joints[i].y, 5, 5);
+    }
+}
+
+function mousePressed() {
+    if (dist(mx, my, joints[joints.length-1].x, joints[joints.length-1].y) < 5){
+        isDraggable = true;
+    }
+}
+
+function mouseReleased() {
+    isDraggable = false;
 }
